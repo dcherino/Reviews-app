8103b0f704de426f7def96d487d7a5847b7d4ecc
Daniel Cherino <daniel@cherino.me>
2020-07-19 12:48:46 +0100

    Adding mocked data

--- src/mock-data.ts
+++ src/mock-data.ts
@@ -1,72 +1,92 @@
 export const mockData = [
-    {
-      name: "Nina Holloway",
-      date: "Mon, 29 Jun 2020 07:40:00 GMT",
-      rating: 4,
-      comment:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus id mi sit amet mattis. Donec mollis tortor sed semper feugiat. Aliquam faucibus quam nec lacus imperdiet, sed euismod justo commodo. Donec porta varius diam et aliquet. \n\n Nulla at nisl a  eros ac, egestas mauris. Mauris fermentum ex erat, ut congue ipsum fermentum ut. Aliquam nec arcu fermentum, tempor urna et, mattis nisl. Aliquam auctor, lectus id ornare bibendum, turpis augue scelerisque massa, a varius nibh neque nec nisi.\n\n",
-    },
-    {
-      name: "Peter Bizmoto",
-      date: "Wed, 01 Jul 2020 12:22:00 GMT",
-      rating: 5,
-      comment:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus id mi sit amet mattis. Donec mollis tortor sed semper feugiat. Aliquam faucibus quam nec lacus imperdiet, sed euismod justo commodo. Donec porta varius diam et aliquet. \n\n Nulla at nisl a  eros ac, egestas mauris. Mauris fermentum ex erat, ut congue ipsum fermentum ut. Aliquam nec arcu fermentum, tempor urna et, mattis nisl. Aliquam auctor, lectus id ornare bibendum, turpis augue scelerisque massa, a varius nibh neque nec nisi.\n\n",
-    },
-    {
-      name: "Henry Smith",
-      date: "Wed, 01 Jul 2020 06:16:00 GMT",
-      rating: 5,
-      comment:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus id mi sit amet mattis. Donec mollis tortor sed semper feugiat. Aliquam faucibus quam nec lacus imperdiet, sed euismod justo commodo. Donec porta varius diam et aliquet. \n\n Nulla at nisl a  eros ac, egestas mauris. Mauris fermentum ex erat, ut congue ipsum fermentum ut. Aliquam nec arcu fermentum, tempor urna et, mattis nisl. Aliquam auctor, lectus id ornare bibendum, turpis augue scelerisque massa, a varius nibh neque nec nisi.\n\n",
-    },
-    {
-      name: "Alicia Harold",
-      date: "Fri, 26 Jun 2020 22:45:00 GMT",
-      rating: 1,
-      comment:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus id mi sit amet mattis. Donec mollis tortor sed semper feugiat. Aliquam faucibus quam nec lacus imperdiet, sed euismod justo commodo. Donec porta varius diam et aliquet. \n\n Nulla at nisl a  eros ac, egestas mauris. Mauris fermentum ex erat, ut congue ipsum fermentum ut. Aliquam nec arcu fermentum, tempor urna et, mattis nisl. Aliquam auctor, lectus id ornare bibendum, turpis augue scelerisque massa, a varius nibh neque nec nisi.\n\n",
-    },
-    {
-      name: "Hope Mikelson",
-      date: "Mon, 13 Jul 2020 08:18:00 GMT",
-      rating: 5,
-      comment:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus id mi sit amet mattis. Donec mollis tortor sed semper feugiat. Aliquam faucibus quam nec lacus imperdiet, sed euismod justo commodo. Donec porta varius diam et aliquet. \n\n Nulla at nisl a  eros ac, egestas mauris. Mauris fermentum ex erat, ut congue ipsum fermentum ut. Aliquam nec arcu fermentum, tempor urna et, mattis nisl. Aliquam auctor, lectus id ornare bibendum, turpis augue scelerisque massa, a varius nibh neque nec nisi.\n\n",
-    },
-    {
-      name: "Tony Soprano",
-      date: "Tue, 02 Jun 2020 10:36:00 GMT",
-      rating: 4,
-      comment:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus id mi sit amet mattis. Donec mollis tortor sed semper feugiat. Aliquam faucibus quam nec lacus imperdiet, sed euismod justo commodo. Donec porta varius diam et aliquet. \n\n Nulla at nisl a  eros ac, egestas mauris. Mauris fermentum ex erat, ut congue ipsum fermentum ut. Aliquam nec arcu fermentum, tempor urna et, mattis nisl. Aliquam auctor, lectus id ornare bibendum, turpis augue scelerisque massa, a varius nibh neque nec nisi.\n\n",
-    },
-    {
-      name: "Larry McCallister",
-      date: "Sat, 16 May 2020 17:22:00 GMT",
-      rating: 2,
-      comment:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus id mi sit amet mattis. Donec mollis tortor sed semper feugiat. Aliquam faucibus quam nec lacus imperdiet, sed euismod justo commodo. Donec porta varius diam et aliquet. \n\n Nulla at nisl a  eros ac, egestas mauris. Mauris fermentum ex erat, ut congue ipsum fermentum ut. Aliquam nec arcu fermentum, tempor urna et, mattis nisl. Aliquam auctor, lectus id ornare bibendum, turpis augue scelerisque massa, a varius nibh neque nec nisi.\n\n",
-    },
-    {
-      name: "Steve McManaman",
-      date: "Wed, 20 May 2020 21:44:00 GMT",
-      rating: 3,
-      comment:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus id mi sit amet mattis. Donec mollis tortor sed semper feugiat. Aliquam faucibus quam nec lacus imperdiet, sed euismod justo commodo. Donec porta varius diam et aliquet. \n\n Nulla at nisl a  eros ac, egestas mauris. Mauris fermentum ex erat, ut congue ipsum fermentum ut. Aliquam nec arcu fermentum, tempor urna et, mattis nisl. Aliquam auctor, lectus id ornare bibendum, turpis augue scelerisque massa, a varius nibh neque nec nisi.\n\n",
-    },
-    {
-      name: "Lina Morgan",
-      date: "Wed, 15 Jul 2020 16:10:00 GMT",
-      rating: 3,
-      comment:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus id mi sit amet mattis. Donec mollis tortor sed semper feugiat. Aliquam faucibus quam nec lacus imperdiet, sed euismod justo commodo. Donec porta varius diam et aliquet. \n\n Nulla at nisl a  eros ac, egestas mauris. Mauris fermentum ex erat, ut congue ipsum fermentum ut. Aliquam nec arcu fermentum, tempor urna et, mattis nisl. Aliquam auctor, lectus id ornare bibendum, turpis augue scelerisque massa, a varius nibh neque nec nisi.\n\n",
-    },
-    {
-      name: "Rick Peterson",
-      date: "Mon, 13 Jul 2020 15:52:00 GMT",
-      rating: 5,
-      comment:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus id mi sit amet mattis. \n\nDonec mollis tortor sed semper feugiat. Aliquam faucibus quam nec lacus imperdiet, sed euismod justo commodo. Donec porta varius diam et aliquet. \n\n Nulla at nisl a  eros ac, egestas mauris. Mauris fermentum ex erat, ut congue ipsum fermentum ut. Aliquam nec arcu fermentum, tempor urna et, mattis nisl. Aliquam auctor, lectus id ornare bibendum, turpis augue scelerisque massa, a varius nibh neque nec nisi.\n\n",
-    },
-  ];
+  {
+    name: "Nina Holloway",
+    email: "test@gmail.com",
+    date: "Mon, 29 Jun 2020 07:40:00 GMT",
+    rating: 4,
+    helpful: 1,
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus id mi sit amet mattis. Donec mollis tortor sed semper feugiat. Aliquam faucibus quam nec lacus imperdiet, sed euismod justo commodo. Donec porta varius diam et aliquet. \n\n Nulla at nisl a  eros ac, egestas mauris. Mauris fermentum ex erat, ut congue ipsum fermentum ut. Aliquam nec arcu fermentum, tempor urna et, mattis nisl. Aliquam auctor, lectus id ornare bibendum, turpis augue scelerisque massa, a varius nibh neque nec nisi.\n\n",
+  },
+  {
+    name: "Peter Bizmoto",
+    email: "test@gmail.com",
+    date: "Wed, 01 Jul 2020 12:22:00 GMT",
+    rating: 5,
+    helpful: 0,
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus id mi sit amet mattis. Donec mollis tortor sed semper feugiat. Aliquam faucibus quam nec lacus imperdiet, sed euismod justo commodo. Donec porta varius diam et aliquet. \n\n Nulla at nisl a  eros ac, egestas mauris. Mauris fermentum ex erat, ut congue ipsum fermentum ut. Aliquam nec arcu fermentum, tempor urna et, mattis nisl. Aliquam auctor, lectus id ornare bibendum, turpis augue scelerisque massa, a varius nibh neque nec nisi.\n\n",
+  },
+  {
+    name: "Henry Smith",
+    email: "test@gmail.com",
+    date: "Wed, 01 Jul 2020 06:16:00 GMT",
+    rating: 5,
+    helpful: 4,
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus id mi sit amet mattis. Donec mollis tortor sed semper feugiat. Aliquam faucibus quam nec lacus imperdiet, sed euismod justo commodo. Donec porta varius diam et aliquet. \n\n Nulla at nisl a  eros ac, egestas mauris. Mauris fermentum ex erat, ut congue ipsum fermentum ut. Aliquam nec arcu fermentum, tempor urna et, mattis nisl. Aliquam auctor, lectus id ornare bibendum, turpis augue scelerisque massa, a varius nibh neque nec nisi.\n\n",
+  },
+  {
+    name: "Alicia Harold",
+    email: "test@gmail.com",
+    date: "Fri, 26 Jun 2020 22:45:00 GMT",
+    rating: 1,
+    helpful: 6,
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus id mi sit amet mattis. Donec mollis tortor sed semper feugiat. Aliquam faucibus quam nec lacus imperdiet, sed euismod justo commodo. Donec porta varius diam et aliquet. \n\n Nulla at nisl a  eros ac, egestas mauris. Mauris fermentum ex erat, ut congue ipsum fermentum ut. Aliquam nec arcu fermentum, tempor urna et, mattis nisl. Aliquam auctor, lectus id ornare bibendum, turpis augue scelerisque massa, a varius nibh neque nec nisi.\n\n",
+  },
+  {
+    name: "Hope Mikelson",
+    email: "test@gmail.com",
+    date: "Mon, 13 Jul 2020 08:18:00 GMT",
+    rating: 5,
+    helpful: 3,
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus id mi sit amet mattis. Donec mollis tortor sed semper feugiat. Aliquam faucibus quam nec lacus imperdiet, sed euismod justo commodo. Donec porta varius diam et aliquet. \n\n Nulla at nisl a  eros ac, egestas mauris. Mauris fermentum ex erat, ut congue ipsum fermentum ut. Aliquam nec arcu fermentum, tempor urna et, mattis nisl. Aliquam auctor, lectus id ornare bibendum, turpis augue scelerisque massa, a varius nibh neque nec nisi.\n\n",
+  },
+  {
+    name: "Tony Soprano",
+    email: "test@gmail.com",
+    date: "Tue, 02 Jun 2020 10:36:00 GMT",
+    rating: 4,
+    helpful: 12,
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus id mi sit amet mattis. Donec mollis tortor sed semper feugiat. Aliquam faucibus quam nec lacus imperdiet, sed euismod justo commodo. Donec porta varius diam et aliquet. \n\n Nulla at nisl a  eros ac, egestas mauris. Mauris fermentum ex erat, ut congue ipsum fermentum ut. Aliquam nec arcu fermentum, tempor urna et, mattis nisl. Aliquam auctor, lectus id ornare bibendum, turpis augue scelerisque massa, a varius nibh neque nec nisi.\n\n",
+  },
+  {
+    name: "Larry McCallister",
+    email: "test@gmail.com",
+    date: "Sat, 16 May 2020 17:22:00 GMT",
+    rating: 2,
+    helpful: 0,
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus id mi sit amet mattis. Donec mollis tortor sed semper feugiat. Aliquam faucibus quam nec lacus imperdiet, sed euismod justo commodo. Donec porta varius diam et aliquet. \n\n Nulla at nisl a  eros ac, egestas mauris. Mauris fermentum ex erat, ut congue ipsum fermentum ut. Aliquam nec arcu fermentum, tempor urna et, mattis nisl. Aliquam auctor, lectus id ornare bibendum, turpis augue scelerisque massa, a varius nibh neque nec nisi.\n\n",
+  },
+  {
+    name: "Steve McManaman",
+    email: "test@gmail.com",
+    date: "Wed, 20 May 2020 21:44:00 GMT",
+    rating: 3,
+    helpful: 0,
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus id mi sit amet mattis. Donec mollis tortor sed semper feugiat. Aliquam faucibus quam nec lacus imperdiet, sed euismod justo commodo. Donec porta varius diam et aliquet. \n\n Nulla at nisl a  eros ac, egestas mauris. Mauris fermentum ex erat, ut congue ipsum fermentum ut. Aliquam nec arcu fermentum, tempor urna et, mattis nisl. Aliquam auctor, lectus id ornare bibendum, turpis augue scelerisque massa, a varius nibh neque nec nisi.\n\n",
+  },
+  {
+    name: "Lina Morgan",
+    email: "test@gmail.com",
+    date: "Wed, 15 Jul 2020 16:10:00 GMT",
+    rating: 3,
+    helpful: 6,
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus id mi sit amet mattis. Donec mollis tortor sed semper feugiat. Aliquam faucibus quam nec lacus imperdiet, sed euismod justo commodo. Donec porta varius diam et aliquet. \n\n Nulla at nisl a  eros ac, egestas mauris. Mauris fermentum ex erat, ut congue ipsum fermentum ut. Aliquam nec arcu fermentum, tempor urna et, mattis nisl. Aliquam auctor, lectus id ornare bibendum, turpis augue scelerisque massa, a varius nibh neque nec nisi.\n\n",
+  },
+  {
+    name: "Rick Peterson",
+    email: "test@gmail.com",
+    date: "Mon, 13 Jul 2020 15:52:00 GMT",
+    rating: 5,
+    helpful: 0,
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus id mi sit amet mattis. \n\nDonec mollis tortor sed semper feugiat. Aliquam faucibus quam nec lacus imperdiet, sed euismod justo commodo. Donec porta varius diam et aliquet. \n\n Nulla at nisl a  eros ac, egestas mauris. Mauris fermentum ex erat, ut congue ipsum fermentum ut. Aliquam nec arcu fermentum, tempor urna et, mattis nisl. Aliquam auctor, lectus id ornare bibendum, turpis augue scelerisque massa, a varius nibh neque nec nisi.\n\n",
+  },
+];
