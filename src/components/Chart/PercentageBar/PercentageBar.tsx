279e336d2aceb398f65abb265339812f4090ebd7
Daniel Cherino <daniel@cherino.me>
2020-07-20 07:50:39 +0100

    Graph with percentage bars feature

---
+++ src/components/Chart/PercentageBar/PercentageBar.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import {
+  ProgressContainer,
+  ProgressBar,
+  ProgressMiddle,
+  ProgressPercentText,
+  ProgressText,
+  ProgressWrap,
+} from "./PercentageBar.styles";
+
+type PercentageBarProps = {
+  starText: string;
+  percentage: number;
+  count: number;
+};
+
+const PercentageBar = ({ starText, percentage, count }: PercentageBarProps) => {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    setValue(percentage);
+  });
+
+  return (
+    <ProgressContainer>
+      <ProgressText>{starText}</ProgressText>
+      <ProgressMiddle>
+        <ProgressWrap>
+          <ProgressBar style={{ width: `${value}%` }} />
+        </ProgressWrap>
+      </ProgressMiddle>
+      <ProgressPercentText>({count})</ProgressPercentText>
+    </ProgressContainer>
+  );
+};
+
+export default PercentageBar;
